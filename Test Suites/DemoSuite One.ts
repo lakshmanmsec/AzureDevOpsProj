d52cb9426f6fcb82f148aacedd3687bb39137bc7
lakshman <lakshman.msec@gmail.com>
2023-07-26 11:24:32 +0530

    Created Test Suite

---
+++ Test Suites/DemoSuite One.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>DemoSuite One</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>80a6b6d7-8805-4f57-bc99-0e165d773fdb</testSuiteGuid>
+</TestSuiteEntity>
