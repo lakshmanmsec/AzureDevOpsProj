9fbcb18ae3d577632c3bdb59bc912ac74478d9eb
lakshman <lakshman.msec@gmail.com>
2023-07-26 12:16:19 +0530

    Added New Test Case called "Testcase Two"

--- Test Suites/DemoSuite One.ts
+++ Test Suites/DemoSuite One.ts
@@ -5,10 +5,17 @@
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
-   <numberOfRerun>0</numberOfRerun>
+   <numberOfRerun>3</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
-   <rerunImmediately>false</rerunImmediately>
+   <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>80a6b6d7-8805-4f57-bc99-0e165d773fdb</testSuiteGuid>
+   <testCaseLink>
+      <guid>ca8a3434-b632-4d68-8960-13e1ce026b98</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test Case One</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
 </TestSuiteEntity>
